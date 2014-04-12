0dd3c9b5588a234add0587744fb4a3df198ac6f5
JoeG <joegoulden@gmail.com>
2014-04-12 11:57:46 +0100

    not support all browsers

--- js/impress.js
+++ js/impress.js
@@ -168,26 +168,26 @@
     var body = document.body;
     
     var ua = navigator.userAgent.toLowerCase();
-    var impressSupported = true;
+    var impressSupported =
                           // browser should support CSS 3D transtorms 
-                          // ( pfx("perspective") !== null ) &&
+                           ( pfx("perspective") !== null ) &&
                            
-                          // and `classList` and `dataset` APIs
-                          // ( body.classList ) &&
-                          // ( body.dataset ) &&
+                           and `classList` and `dataset` APIs
+                           ( body.classList ) &&
+                           ( body.dataset ) &&
                            
                           // but some mobile devices need to be blacklisted,
                           // because their CSS 3D support or hardware is not
                           // good enough to run impress.js properly, sorry...
-                          // ( ua.search(/(iphone)|(ipod)|(android)/) === -1 );
+                           ( ua.search(/(iphone)|(ipod)|(android)/) === -1 );
     
-   // if (!impressSupported) {
+    if (!impressSupported) {
         // we can't be sure that `classList` is supported
-    //    body.className += " impress-not-supported ";
-   // } else {
+        body.className += " impress-not-supported ";
+    } else {
         body.classList.remove("impress-not-supported");
         body.classList.add("impress-supported");
-   // }
+    }
     
     // GLOBALS AND DEFAULTS
     
